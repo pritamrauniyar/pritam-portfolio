56339958ed6be710c335ba6669ec7e1192dacb8f
pritamrauniyar <pritamrauniyar.np@gmail.com>
2024-11-26 17:43:25 +0530

    Updated resume, removed commented code, update favicon and meta data

--- src/App.js
+++ src/App.js
@@ -7,32 +7,7 @@ import Project from "./pages/Project/Project";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home/Home";
 import { MyContextProvider } from "./components/MyContext/MyContext";
-//import { Provider } from "react-redux";
-//import { store } from "../src/components/Redux/Store";
 function App() {
-  //   return (
-  //     // <MyContextProvider>
-  //     <Provider store={store}>
-  //       <Router>
-  //         <div className="App">
-  //           <Navbar />
-  //           <main>
-  //             <Routes>
-  //               <Route path="/" element={<Home />} />
-  //               <Route path="/project" element={<Project />} />
-  //               <Route path="/about" element={<About />} />
-  //               <Route path="/contact" element={<Contact />} />
-  //             </Routes>
-  //           </main>
-  //           <Footer />
-  //         </div>
-  //       </Router>
-  //     </Provider>
-  //     // </MyContextProvider>
-  //   );
-  // }
-
-  // export default App;
 
   return (
     <MyContextProvider>
--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -8,13 +8,24 @@ const HeroSection = () => {
 
   useEffect(() => {
     const typed = new Typed(heroAnimation.current, {
-      strings: ["Software Developer", "Web Developer", "Team Player"], // Strings to display
-      typeSpeed: 50, // Typing speed
-      backSpeed: 50, // Backspace speed
-      loop: true, // Repeat animation
+      strings: [
+        "Software Development Engineer",
+        "Web Developer",
+        "Team Player",
+        "Frontend Enthusiast",
+        "Backend Problem Solver",
+        "Passionate Coder",
+        "React Developer",
+        "Angular Developer",
+        "UI Designer",
+        "Tech Optimizer",
+        "Agile Practitioner"
+      ],
+      typeSpeed: 50, 
+      backSpeed: 50, 
+      loop: true,
     });
 
-    // Cleanup for component unmount
     return () => {
       typed.destroy();
     };
@@ -22,7 +33,7 @@ const HeroSection = () => {
   return (
     <div className="herosection-container">
       <div className="left-section">
-        Hi, My name is <span className="text-purple">Pritam</span>
+        Hi, I am <span className="text-purple">Pritam</span>
         <div>and I am a Passionate </div>
         <div id="heroAnimation">
           {" "}
